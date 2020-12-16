e7d8a14eab9d85e730d03404709d96fbbd8375f8
Sankalpa Dahal Makarov <62802864+Sankalpa683@users.noreply.github.com>
2020-12-16 16:28:06 +0545

    Update db.php

--- chat/backend/db.php
+++ chat/backend/db.php
@@ -1,4 +1,4 @@
 <?php 
 
-$conn = mysqli_connect("sql205.byetcluster.com","epiz_27468584","JOyjUwyLeY9rW","epiz_27468584_SankuzMessage");
+$conn = mysqli_connect("localhost","root","","SankuzMessage");
 ?>
